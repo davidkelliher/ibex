08b1d985c85cd369450fa4e2a4e1848af4ab9a92
David Kelliher <David.Kelliher@gmail.com>
2018-11-06 22:54:36 +0000

    Improve tune_to_voltage.py

--- scripts/tune_to_voltages.py
+++ scripts/tune_to_voltages.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python
 from __future__ import division
-import math 
 import sys
 import numpy as np
 import pylab as plt
@@ -10,6 +9,15 @@ import time
 import ibex_optics
 
 #for a given set of desired cell tunes, find the corresponding voltages
+#usage
+#
+#To find the voltage required in the case of nu_x=nu_y
+#./tune_to_voltage.py <nux> 
+#
+#To find the voltage required in the case of nu_x!=nu_y
+#./tune_to_voltage.py <nux> <nuy> 
+#
+
 op = ibex_optics.optics(f_rf=1.0, npts=1000)
 
 if len(sys.argv) == 1:
@@ -26,6 +34,17 @@ elif len(sys.argv) == 3:
 	
 opt_method =  'L-BFGS-B'#'Nelder-Mead' # also good
 
+def volt_root_1D(vg):	
+	
+	nux = op.voltage_to_tune(vg)[0]
+	
+	if nux != None:
+		nud = nux - nugx
+	else:
+		nud = 1e6
+
+	return nud	
+	
 def volt_root_2D(v_a):
 	nux, nuy = op.voltage_to_tune(v_a[0],v_a[1])
 	try:
@@ -34,24 +53,28 @@ def volt_root_2D(v_a):
 		nud = 1e6
 	return nud
 
-def volt_root_1D(vg):
-
-	
-	nud = op.voltage_to_tune(vg)[0] - nugx
-
 		
-	return nud				
-
 #initial guess
 vg = 50
 ug = 0
 
+#if requested tunes are equal, first try Newton
+
 time0 = time.time()
 if nugx == nugy:
-	res = newton(volt_root_1D, vg)
-	v_out = res
-	u_out = 0
+	try:
+		print "Equal tunes - try Newton-Raphson algorithm"
+		res = newton(volt_root_1D, vg)
+		v_out = res
+		u_out = 0
+	except:
+		print "no solution found. Try "+opt_method+" algorithm"
+		ini_a = np.array([vg,ug])
+		res = minimize(volt_root_2D, ini_a, method=opt_method)
+		v_out, u_out = res['x']
+		print "message: ",res['message']
 else:
+	print "Try "+opt_method+" algorithm"
 	ini_a = np.array([vg,ug])
 	res = minimize(volt_root_2D, ini_a, method=opt_method)
 	v_out, u_out = res['x']
@@ -60,11 +83,10 @@ else:
 time1 = time.time()
 
 
-
 tune_out = op.voltage_to_tune(v_out,u_out)
 		
 		
 print "obtained voltages ",tune_out
 print "optimisation time ",time1-time0
-print "rf voltage V0 = ",v_out
-print "DC voltage U0 = ",u_out
+print "RF voltage, V0 = ",v_out
+print "DC voltage, U0 = ",u_out
